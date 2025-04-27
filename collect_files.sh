62013322d2bdc19e03769f1a4a50d354bf9a4e3a
root <root@sqepzgzdgc.local>
2025-04-27 10:17:40 +0000

    pop12

--- collect_files.sh
+++ collect_files.sh
@@ -5,7 +5,4 @@ import os
 import shutil
 import_dir = sys.argv[1]
 output_dir = sys.argv[2]
-for frst, sec, thrd in os.walk(import_dir):
-    for chich in thrd:
-        shutil.copy(frst + '/' + chich, output_dir + '/' + chich)
 ' "$1" "$2"
