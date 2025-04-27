36673ff30b2539d5353d2ce5d4e39840112e1324
root <root@sqepzgzdgc.local>
2025-04-27 10:12:17 +0000

    pop9

--- collect_files.sh
+++ collect_files.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 python3 -c '
-import sys
-dir1 = sys.argv[1]
-dir2 = sys.argv[2]
+import sys, os, shutil
+import_dir = sys.argv[1]
+output_dir = sys.argv[2]
+for frst, sec, thrd in os.walk(import_dir):
+    for chich in thrd:
+        shutil.copy(frst + '/' + chich, output_dir + '/' + chich)
 ' "$1" "$2"
