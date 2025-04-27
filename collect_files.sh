c09e58f7c786852f14c905b721266a65065db5ba
root <root@sqepzgzdgc.local>
2025-04-27 12:29:51 +0000

    pop30

--- collect_files.sh
+++ collect_files.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 if [ "$1" == "--max_depth" ]; then
-    max_depth=$2
     import_dir=$3
     output_dir=$4
+    max_depth=$2
     python3 -c '
 import sys
 import os
 import shutil
-max_depth = int(sys.argv[1])
 import_dir = sys.argv[2]
 output_dir = sys.argv[3]
+max_depth = int(sys.argv[1])
 def main(now, dim1, dim2):
     if now > max_depth:
         return
     for i in os.listdir(dim1):
         shutil.copy(os.path.join(dim1, i), dim2)
 main(1, import_dir, output_dir)
-' "$import_dir" "$output_dir" "$max_depth"
+' "$import_dir" "$output_dir"
 fi
