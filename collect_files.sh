4609a1f4a7f62b6344138643fa07a47fdeaf5c42
root <root@sqepzgzdgc.local>
2025-04-27 11:35:51 +0000

    pop16

--- collect_files.sh
+++ collect_files.sh
@@ -8,8 +8,9 @@ output_dir = sys.argv[2]
 max_depth = int(sys.argv[4])
 flag = 0
 for frst, sec, thrd in os.walk(import_dir):
-    if flag < max_depth:
-        flag += 1
-        for chich in thrd:
-            shutil.copy(os.path.join(frst, chich), os.path.join(output_dir, chich))
+    now = frst[len(import_dir):].count(os.sep)
+    if now >= max_depth:
+        continue
+    for chich in thrd:
+        shutil.copy(os.path.join(frst, chich), os.path.join(output_dir, chich))
 ' "$1" "$2"
