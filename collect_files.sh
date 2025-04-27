0ea1416c39170254b9ea885ddc175af7bbfd44a8
root <root@sqepzgzdgc.local>
2025-04-27 12:32:52 +0000

    pop31

--- collect_files.sh
+++ collect_files.sh
@@ -13,6 +13,8 @@ max_depth = int(sys.argv[1])
 def main(now, dim1, dim2):
     if now > max_depth:
         return
+    if not os.path.exists(dim2):
+        os.makedirs(dim2)
     for i in os.listdir(dim1):
         shutil.copy(os.path.join(dim1, i), dim2)
 main(1, import_dir, output_dir)
