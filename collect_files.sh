2685e748b3d0a7a3b4638d216cfa26d49c7ac38f
root <root@sqepzgzdgc.local>
2025-04-27 11:32:15 +0000

    pop15

--- collect_files.sh
+++ collect_files.sh
@@ -5,10 +5,10 @@ import os
 import shutil
 import_dir = sys.argv[1]
 output_dir = sys.argv[2]
---max_depth = int(sys.argv[4])
+max_depth = int(sys.argv[4])
 flag = 0
 for frst, sec, thrd in os.walk(import_dir):
-    if flag < --max_depth:
+    if flag < max_depth:
         flag += 1
         for chich in thrd:
             shutil.copy(os.path.join(frst, chich), os.path.join(output_dir, chich))
